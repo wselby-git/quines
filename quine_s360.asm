e0052c63f10c37e311c9e8a9e1134ec0b189581c
wselby <122949882+wselby-git@users.noreply.github.com>
2023-03-12 15:18:32 -0600

    Create quine_s360.asm

---
+++ quine_s360.asm
@@ -0,0 +1,23 @@
+         PRINT NOGEN
+START    EQU   *
+
+         DC    C'         PRINT NOGEN'
+         DC    X'0A'
+         DC    C'START    EQU   *'
+         DC    X'0A'
+         DC    C'         DC    C'''
+         DC    C'         DC    X'''
+         DC    C'0A'''
+         DC    X'0A'
+         DC    C'         DC    C'''
+         DC    C'         DC    X'''
+         DC    C'START    EQU   *'
+         DC    X'0A'
+         DC    C'         DC    C'''
+         DC    C'         DC    X'''
+         DC    C'0A'''
+         DC    X'0A'
+         DC    C'         END'
+         DC    X'0A'
+
+         END
